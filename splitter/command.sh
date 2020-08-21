abdc72c59fa819a332794c632278a8c641f8cb77
Wale Joseph <30349247+josoga2@users.noreply.github.com>
2020-08-21 17:00:36 +0300

    Update command.sh

--- splitter/command.sh
+++ splitter/command.sh
@@ -1,5 +1,8 @@
 for file in *.bed
 do
   rgt-TDF regiontest -r nfixedout.fa -bed $file -rn ecCEBPA -organism hg19 -o genomic_region_test/$file -n 100 -l 10
+  
+  rgt-TDF regiontest -r nfixedout.fa -bed probes_mut_bed.bed -f probes_nonmut_bed.bed -fr off -rn ecCEBPA -organism hg19 -o genomic_region_test/ecCEBPA -n 100 -l 10 -e 20 -c 2
+  
 done
 
